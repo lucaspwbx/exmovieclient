069efa0a646f358ca614fde0ac9eb407055e8296
Lucas Weiblen <lucasweiblen@gmail.com>
2014-05-18 19:29:59 -0300

    Improve pattern matching

--- lib/movies/cli.ex
+++ lib/movies/cli.ex
@@ -4,12 +4,11 @@ defmodule MovieClient.CLI do
   end
 
   def parse_args(argv) do
-    parse = OptionParser.parse(argv)
+    { [ {_, framework } ], _, _ } = OptionParser.parse(argv)
 
-    case parse do
-      { [ {_,:sinatra} ], _, _ } -> 9292
-      { [ {_,:chicago_boss} ], _, _ } -> 8001
-
+    case framework do
+      :sinatra -> 9292
+      :chicago_boss -> 8001
       _ -> IO.puts "No match for framework"
     end
   end
